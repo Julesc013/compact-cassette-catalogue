49126287bc18efd19995f13d2e72feef81e62e7e
Jules Carboni <30209022+Julesc013@users.noreply.github.com>
2020-04-21 23:31:47 +1000

    added installer project

---
+++ Compact Cassette Catalogue Installer/frmMain.Designer.vb
@@ -0,0 +1,121 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
+        Me.pnlButtons = New System.Windows.Forms.Panel()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.btnBack = New System.Windows.Forms.Button()
+        Me.btnNext = New System.Windows.Forms.Button()
+        Me.picSide = New System.Windows.Forms.PictureBox()
+        Me.pnlButtons.SuspendLayout()
+        CType(Me.picSide, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'pnlButtons
+        '
+        Me.pnlButtons.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.pnlButtons.BackColor = System.Drawing.SystemColors.Control
+        Me.pnlButtons.Controls.Add(Me.btnCancel)
+        Me.pnlButtons.Controls.Add(Me.btnBack)
+        Me.pnlButtons.Controls.Add(Me.btnNext)
+        Me.pnlButtons.Location = New System.Drawing.Point(0, 433)
+        Me.pnlButtons.Margin = New System.Windows.Forms.Padding(0)
+        Me.pnlButtons.Name = "pnlButtons"
+        Me.pnlButtons.Size = New System.Drawing.Size(732, 70)
+        Me.pnlButtons.TabIndex = 0
+        '
+        'btnCancel
+        '
+        Me.btnCancel.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnCancel.Location = New System.Drawing.Point(612, 20)
+        Me.btnCancel.Margin = New System.Windows.Forms.Padding(20)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(100, 30)
+        Me.btnCancel.TabIndex = 2
+        Me.btnCancel.Text = "Cancel"
+        Me.btnCancel.UseVisualStyleBackColor = True
+        '
+        'btnBack
+        '
+        Me.btnBack.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnBack.Enabled = False
+        Me.btnBack.Location = New System.Drawing.Point(392, 20)
+        Me.btnBack.Margin = New System.Windows.Forms.Padding(0, 20, 0, 20)
+        Me.btnBack.Name = "btnBack"
+        Me.btnBack.Size = New System.Drawing.Size(100, 30)
+        Me.btnBack.TabIndex = 1
+        Me.btnBack.Text = "Back"
+        Me.btnBack.UseVisualStyleBackColor = True
+        '
+        'btnNext
+        '
+        Me.btnNext.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnNext.Location = New System.Drawing.Point(492, 20)
+        Me.btnNext.Margin = New System.Windows.Forms.Padding(0, 20, 0, 20)
+        Me.btnNext.Name = "btnNext"
+        Me.btnNext.Size = New System.Drawing.Size(100, 30)
+        Me.btnNext.TabIndex = 0
+        Me.btnNext.Text = "Next"
+        Me.btnNext.UseVisualStyleBackColor = True
+        '
+        'picSide
+        '
+        Me.picSide.Image = CType(resources.GetObject("picSide.Image"), System.Drawing.Image)
+        Me.picSide.Location = New System.Drawing.Point(0, 0)
+        Me.picSide.Margin = New System.Windows.Forms.Padding(0)
+        Me.picSide.Name = "picSide"
+        Me.picSide.Size = New System.Drawing.Size(210, 433)
+        Me.picSide.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picSide.TabIndex = 1
+        Me.picSide.TabStop = False
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.ClientSize = New System.Drawing.Size(732, 503)
+        Me.Controls.Add(Me.picSide)
+        Me.Controls.Add(Me.pnlButtons)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "frmMain"
+        Me.Text = "Compact Cassette Catalogue Installer"
+        Me.pnlButtons.ResumeLayout(False)
+        CType(Me.picSide, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents pnlButtons As Panel
+    Friend WithEvents btnCancel As Button
+    Friend WithEvents btnBack As Button
+    Friend WithEvents btnNext As Button
+    Friend WithEvents picSide As PictureBox
+End Class
